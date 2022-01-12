b3b81a79357e9a36b42bd70cb7b9743f9fc4c1f9
oleg0031 <oleg0031@gmail.com>
2022-01-12 16:46:15 +0200

    Correctly implemented game mechanics

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,12 +6,15 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<Transform> containers;
-    private Animator animator1, animator2, animator3;
+    private Animator animator1, animator2, animator3, coinAnimator;
+    private int coinIndex;
     void Start()
     {
          animator1 = containers[0].GetComponent<Animator>(); 
          animator2 = containers[1].GetComponent<Animator>();
          animator3 = containers[2].GetComponent<Animator>();
+         coinAnimator = GameObject.Find("Coin").GetComponent<Animator>();
+         coinIndex = 1;
     }
 
     public void StartGame()
@@ -31,7 +34,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator Shuffle()
     {
-        while (true)
+        for (int i = 0; i < 10; i++)
         {
             yield return new WaitForSeconds(1);
             int randomContainer1 = Random.Range(0, 3);
@@ -55,30 +58,59 @@ public class GameManager : MonoBehaviour
                 else
                     Swap(1, 2);
             }
-            
         }
     }
 
     void Swap(int index1, int index2)
     {
+        print("Coin index before swap:" + coinIndex);
         if (index1 == 0 && index2 == 1 || index1 == 1 && index2 == 0)
         {
-            animator1.Play("Container anim1");
-            animator2.Play("Container anim4");
+            animator1.CrossFadeInFixedTime("Container anim1", 0);
+            animator2.CrossFadeInFixedTime("Container anim4", 0);
             animator3.Play("Container3 Idle");
+            if (coinIndex == 1)
+            {
+                coinAnimator.CrossFadeInFixedTime("Coin MoveLeft", 0);
+                coinIndex = 0;
+            }
+            else if (coinIndex == 0)
+            {
+                coinAnimator.CrossFadeInFixedTime("Coin LeftRight", 0);
+                coinIndex = 1;
+            }
         }
         else if (index1 == 0 && index2 == 2 || index1 == 2 && index2 == 0)
         {
-            animator1.Play("Container anim5");
+            animator1.CrossFadeInFixedTime("Container anim5", 0);
             animator2.Play("Container2 Idle");
-            animator3.Play("Container anim6");
+            animator3.CrossFadeInFixedTime("Container anim6", 0);
+            if (coinIndex == 2)
+            {
+                coinAnimator.CrossFadeInFixedTime("Coin MoveLeft2", 0);
+                coinIndex = 0;
+            }
+            else if (coinIndex == 0)
+            {
+                coinAnimator.CrossFadeInFixedTime("Coin MoveRight2", 0);
+                coinIndex = 2;
+            }
         }
         else if (index1 == 1 && index2 == 2 || index1 == 2 && index2 == 1)
         {
             animator1.Play("Container1 Idle");
-            animator2.Play("Container anim2");
-            animator3.Play("Container anim3");
+            animator2.CrossFadeInFixedTime("Container anim2", 0);
+            animator3.CrossFadeInFixedTime("Container anim3", 0);
+            if (coinIndex == 2)
+            {
+                coinAnimator.CrossFadeInFixedTime("Coin RightLeft", 0);
+                coinIndex = 1;
+            }
+            else if (coinIndex == 1)
+            {
+                coinAnimator.CrossFadeInFixedTime("Coin MoveRight", 0);
+                coinIndex = 2;
+            }
         }
-        print($"{index1} swapped with {index2}");
     }
 }
