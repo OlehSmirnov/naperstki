1cf298e1543084a951262f7e8e19b461c7057a6e
oleg0031 <oleg0031@gmail.com>
2022-01-12 10:27:14 +0200

    Added SFX pack, added animations for containers

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,10 +19,47 @@ public class GameManager : MonoBehaviour
             t.position = new Vector2(container.x, -1.1f);
         }
         Destroy(GameObject.Find("Play"));
+        StartCoroutine(Shuffle());
     }
 
     public void ResetGame()
     {
         SceneManager.LoadScene("Gameplay");
+    }
+
+    IEnumerator Shuffle()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            int randomContainer1 = Random.Range(0, 3);
+            int randomContainer2;
+            if (randomContainer1 == 0)
+            {
+                randomContainer2 = Random.Range(1, 3);
+                Swap(0, randomContainer2);
+                
+            }
+            else if (randomContainer1 == 2)
+            {
+                randomContainer2 = Random.Range(0, 2);
+                Swap(2, randomContainer2);
+            }
+            else
+            {
+                randomContainer2 = Random.Range(0, 2);
+                if (randomContainer2 == 0)
+                    Swap(1, 0);
+                else
+                    Swap(1, 2);
+            }
+            
+        }
+    }
+
+    void Swap(int index1, int index2)
+    {
+        (containers[index1], containers[index2]) = (containers[index2], containers[index1]);
+        print($"{index1} swapped with {index2}");
     }
 }
